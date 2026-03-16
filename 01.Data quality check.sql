a0c27d7d429c7d9bcf96bed1381adb7ef79a0ad5
Purity K <puritykoima2020@gmail.com>
2026-03-16 03:30:32 +0300

    Delete 01.Data quality check.sql

--- 01.Data quality check.sql
+++
@@ -1,6 +0,0 @@
--- Data quality check. This query checks for null values.
-SELECT
-    COUNT(*) AS total_records,
-    SUM(CASE WHEN u2_vaccine_late_missed IS NULL THEN 1 ELSE 0 END) AS missing_vaccine_status,
-    SUM(CASE WHEN u2_no_provider_why IS NULL THEN 1 ELSE 0 END) AS missing_provider_reason
-FROM practice_db.kenya_ihme_prem_ch_health;
